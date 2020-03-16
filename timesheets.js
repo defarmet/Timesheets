e99a1cf60e5b3b759c2283da6737146faabf8d63
defarmet <defarmet@protonmail.com>
2020-03-16 18:50:38 -0400

    Added firebase init

---
+++ timesheets.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "AIzaSyCmzRvRdms3oR6Gwn-mt8A7nZErNxrxfjI",
+	authDomain: "test-project-56ae5.firebaseapp.com",
+	databaseURL: "https://test-project-56ae5.firebaseio.com",
+	projectId: "test-project-56ae5",
+	storageBucket: "test-project-56ae5.appspot.com"
+};
+firebase.initializeApp(config);
